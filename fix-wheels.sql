60670c8bcde94afd04584b08edcde3ea332dd068
Sven Rosema <rosema.sven@gmail.com>
2026-02-12 15:29:46 +0100

    Remove temporary fix-wheels.sql

--- fix-wheels.sql
+++
@@ -1,5 +0,0 @@
--- Fix wheel labels in test database
-UPDATE "Option" SET label = '18"' WHERE type = 'wheels' AND value = '18';
-UPDATE "Option" SET label = '19"' WHERE type = 'wheels' AND value = '19';
-UPDATE "Option" SET label = '20"' WHERE type = 'wheels' AND value = '20';
-UPDATE "Option" SET label = '21"' WHERE type = 'wheels' AND value = '21';
